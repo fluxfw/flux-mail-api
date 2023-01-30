4c76af1d2138191d7a06075552126f2441066cfc
fluxfw <fluxfw@fluxlabs.ch>
2023-01-30 13:09:23 +0100

    Remove build archive

--- src/autoload.php
+++ src/autoload.php
@@ -2,34 +2,8 @@
 
 namespace FluxMailApi;
 
-require_once __DIR__ . "/../libs/flux-autoload-api/autoload.php";
-
-use FluxMailApi\Libs\FluxAutoloadApi\Adapter\Autoload\Psr4Autoload;
-use FluxMailApi\Libs\FluxAutoloadApi\Adapter\Checker\PhpExtChecker;
-use FluxMailApi\Libs\FluxAutoloadApi\Adapter\Checker\PhpVersionChecker;
-
-PhpVersionChecker::new(
-    ">=8.2"
-)
-    ->checkAndDie(
-        __NAMESPACE__
-    );
-PhpExtChecker::new(
-    [
-        "imap",
-        "json"
-    ]
-)
-    ->checkAndDie(
-        __NAMESPACE__
-    );
-
-Psr4Autoload::new(
-    [
-        __NAMESPACE__ => __DIR__
-    ]
-)
-    ->autoload();
-
-require_once __DIR__ . "/../libs/php-imap/vendor/autoload.php";
-require_once __DIR__ . "/../libs/PHPMailer/vendor/autoload.php";
+spl_autoload_register(function (string $class) : void {
+    if (str_starts_with($class, __NAMESPACE__ . "\\")) {
+        require_once __DIR__ . str_replace("\\", "/", substr($class, strlen(__NAMESPACE__))) . ".php";
+    }
+});
